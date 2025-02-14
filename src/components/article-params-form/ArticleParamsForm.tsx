a9b9780f7eaa1f19f89ce42086e221f7ed9e6f11
boshan <ziijey@yandex.ru>
2025-02-15 00:52:01 +0300

    *форма и функционал реализован

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,30 +1,35 @@
+import clsx from 'clsx';
+
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
-import clsx from 'clsx';
-import { useState, FormEvent, useRef, useEffect } from 'react';
-
+import { useState, useRef, FormEvent, useEffect, useCallback } from 'react';
+import { Select } from 'src/ui//select';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
 import {
+	fontFamilyOptions,
+	fontSizeOptions,
+	fontColors,
+	contentWidthArr,
+	backgroundColors,
 	defaultArticleState,
 	ArticleStateType,
 } from 'src/constants/articleProps';
 
-type sideProps = {
-	isOpen: boolean;
-	setIsOpen: (open: boolean) => void;
-};
-
 interface IArticleParamsFormProps {
 	articleState: ArticleStateType;
 	setArticleState: (param: ArticleStateType) => void;
+	isOpen: boolean;
+	setIsOpen: (open: boolean) => void;
 }
 
 export const ArticleParamsForm = ({
 	setArticleState,
-	isOpen,
-	setIsOpen,
-}: IArticleParamsFormProps & sideProps) => {
+}: IArticleParamsFormProps) => {
+	const [isOpen, setIsMenuOpen] = useState<boolean>(false);
 	const [formState, setFormState] = useState({
 		fontFamily: defaultArticleState.fontFamilyOption,
 		fontSize: defaultArticleState.fontSizeOption,
@@ -33,7 +38,16 @@ export const ArticleParamsForm = ({
 		contentWidth: defaultArticleState.contentWidth,
 	});
 
-	function reloadState() {
+	const menuRef = useRef<HTMLFormElement | null>(null);
+
+	const toggleOpenForm = useCallback(
+		(bool?: boolean): void => {
+			setIsMenuOpen(bool !== undefined ? bool : !isOpen);
+		},
+		[isOpen]
+	);
+
+	const reloadState = useCallback(() => {
 		setFormState({
 			fontFamily: defaultArticleState.fontFamilyOption,
 			fontSize: defaultArticleState.fontSizeOption,
@@ -42,11 +56,20 @@ export const ArticleParamsForm = ({
 			contentWidth: defaultArticleState.contentWidth,
 		});
 		setArticleState(defaultArticleState);
-	}
+	}, [setArticleState]);
+
+	const handleChange = useCallback(
+		(field: keyof typeof formState) => (selectOption: any) => {
+			setFormState((prevState) => ({
+				...prevState,
+				[field]: selectOption,
+			}));
+		},
+		[]
+	);
 
 	const formSubmitHandler = (evt: FormEvent) => {
 		evt.preventDefault();
-
 		setArticleState({
 			fontFamilyOption: formState.fontFamily,
 			fontSizeOption: formState.fontSize,
@@ -54,35 +77,66 @@ export const ArticleParamsForm = ({
 			backgroundColor: formState.backgroundColor,
 			contentWidth: formState.contentWidth,
 		});
-
-		setIsOpen(false); // Теперь `setIsOpen` передаётся в пропсах и ошибки не будет
+		setIsMenuOpen(false);
 	};
-
-	const menuRef = useRef<HTMLFormElement | null>(null);
 
 	useEffect(() => {
 		const handleOutsideClick = (event: MouseEvent) => {
-			const element = event.target as HTMLElement;
-			if (menuRef.current && !menuRef.current.contains(element)) {
-				setIsOpen(false);
+			if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+				setIsMenuOpen(false);
 			}
 		};
 
 		document.addEventListener('mousedown', handleOutsideClick);
-		return () => {
-			document.removeEventListener('mousedown', handleOutsideClick);
-		};
+		return () => document.removeEventListener('mousedown', handleOutsideClick);
 	}, []);
 
+	console.log(isOpen);
 	return (
 		<>
-			<ArrowButton isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
+			<ArrowButton onClick={() => toggleOpenForm()} isOpen={isOpen} />
 			<aside
-				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
+				className={clsx(styles.container, isOpen && styles.container_open)}
+				ref={menuRef}>
 				<form
 					className={styles.form}
-					onReset={reloadState}
-					onSubmit={formSubmitHandler}>
+					onSubmit={formSubmitHandler}
+					onReset={reloadState}>
+					<Text as={'h2'} size={31} weight={800} uppercase={true}>
+						Задайте параметры
+					</Text>
+					<Select
+						title='Шрифт'
+						selected={formState.fontFamily}
+						options={fontFamilyOptions}
+						onChange={handleChange('fontFamily')}
+					/>
+					<RadioGroup
+						title='Размер шрифта'
+						name='Размер шрифта'
+						selected={formState.fontSize}
+						options={fontSizeOptions}
+						onChange={handleChange('fontSize')}
+					/>
+					<Select
+						title='Цвет шрифта'
+						selected={formState.fontColor}
+						options={fontColors}
+						onChange={handleChange('fontColor')}
+					/>
+					<Separator />
+					<Select
+						title='Цвет фона'
+						selected={formState.backgroundColor}
+						options={backgroundColors}
+						onChange={handleChange('backgroundColor')}
+					/>
+					<Select
+						title='Ширина контента'
+						selected={formState.contentWidth}
+						options={contentWidthArr}
+						onChange={handleChange('contentWidth')}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
